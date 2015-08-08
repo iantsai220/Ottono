7bca0fd0ddd921f4cc3933cb16d41ef813006d95
iantsai <iantsai86@gmail.com>
2015-08-08 16:12:12 -0700

    Initial Commit

---
+++ Ottono - Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ottono - Swift
+//
+//  Created by Ian Tsai on 2015-08-08.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
